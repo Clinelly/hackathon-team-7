b617bc5a3abe88736fbb7b7e0db64dea9904fbe4
MarcelloMuy <marcellomuylaert@icloud.com>
2022-11-19 21:51:12 +0000

    Create function to generate keyboard

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 //Global variables
 const question = document.getElementById("question-content");
 
+// List of worlds
+const words = ["css", "dublin", "javascript", "code"]
+
 let questions = [{
     question: 'In which FIFA world cup was "Waka Waka" played?',
     choice1: '2002',
@@ -22,6 +25,7 @@ let questions = [{
 // start the quiz
 let startQuiz = () => {
     availableQuestions = [...questions];
+    loadKeyboard();
     getNewQuestion();
 }
 
@@ -30,5 +34,18 @@ let getNewQuestion = () => {
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
     question.innerText = currentQuestion.question;
+}
+// display keyboard
+function loadKeyboard() {
+    //Select a random world from from list
+    let selectedWord = words[Math.floor(Math.random() * words.length)];
+    //Create keys
+    for (let i = 0; i < selectedWord.length; i++) {
+        let newKey = document.createElement('div');
+        newKey.setAttribute('id','keyboard'+i);
+        let keyboardArea = document.getElementById('keyboard-area');
+        newKey.classList.add('col', 'border', 'border-dark', 'text-center', 'key-style');
+        keyboardArea.appendChild(newKey);
+    }    
 }
 startQuiz()
